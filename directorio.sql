fd8e99289b44cce8d6c08538be71fcef363511ad
jhanner <jaurena8@misena.edu.co>
2018-12-26 22:58:27 -0500

    base de datos

---
+++ directorio.sql
@@ -0,0 +1,286 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-12-2018 a las 04:55:48
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `directorio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `barrio`
+--
+
+CREATE TABLE `barrio` (
+  `barrio_id` int(11) NOT NULL,
+  `descripcion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `comuna` int(11) NOT NULL,
+  `latitud` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `logitud` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `ciudad_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `categoria_id` int(11) NOT NULL,
+  `nombre_cat` varchar(30) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudad`
+--
+
+CREATE TABLE `ciudad` (
+  `ciudad_id` int(11) NOT NULL,
+  `descripcion` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `departamento_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `departamento_id` int(11) NOT NULL,
+  `descripcio` varchar(30) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresa`
+--
+
+CREATE TABLE `empresa` (
+  `empresa_id` int(11) NOT NULL,
+  `nit` int(11) NOT NULL,
+  `razon_social` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` text COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
+  `direccion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `celular` int(11) NOT NULL,
+  `email` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `estado_registro` tinyint(4) NOT NULL,
+  `subcategoria_id` int(11) NOT NULL,
+  `estado_pago` tinyint(1) NOT NULL,
+  `contador_emp` bigint(20) NOT NULL,
+  `barrio_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagen`
+--
+
+CREATE TABLE `imagen` (
+  `imagen-id` int(11) NOT NULL,
+  `descripcion` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `ruta_url` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `empresa-id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subcategoria`
+--
+
+CREATE TABLE `subcategoria` (
+  `subcategoria_id` int(11) NOT NULL,
+  `nombre_sub_cat` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `descripcion_sub_cat` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `contador-sub_cat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usuario`
+--
+
+CREATE TABLE `tipo_usuario` (
+  `tipo_usuario_id` int(11) NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `usuario_id` int(11) NOT NULL,
+  `num_doc` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `tipo_doc` text COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` text COLLATE utf8_spanish_ci NOT NULL,
+  `apellido` text COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `sexo` text COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `direccion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `email` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `tipo_usuario_id` int(11) NOT NULL,
+  `usuario` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
+  `contraseña` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `barrio`
+--
+ALTER TABLE `barrio`
+  ADD PRIMARY KEY (`barrio_id`),
+  ADD KEY `ciudad_id` (`ciudad_id`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`categoria_id`);
+
+--
+-- Indices de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD PRIMARY KEY (`ciudad_id`),
+  ADD KEY `departamento_id` (`departamento_id`);
+
+--
+-- Indices de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`departamento_id`);
+
+--
+-- Indices de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`empresa_id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `usuario_id_2` (`usuario_id`),
+  ADD KEY `subcategoria_id` (`subcategoria_id`),
+  ADD KEY `barrio_id` (`barrio_id`);
+
+--
+-- Indices de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD PRIMARY KEY (`imagen-id`),
+  ADD KEY `empresa-id` (`empresa-id`);
+
+--
+-- Indices de la tabla `subcategoria`
+--
+ALTER TABLE `subcategoria`
+  ADD PRIMARY KEY (`subcategoria_id`),
+  ADD KEY `categoria_id` (`categoria_id`);
+
+--
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`tipo_usuario_id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`usuario_id`),
+  ADD KEY `tipo_usuario_id` (`tipo_usuario_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  MODIFY `tipo_usuario_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `barrio`
+--
+ALTER TABLE `barrio`
+  ADD CONSTRAINT `barrio_ibfk_1` FOREIGN KEY (`ciudad_id`) REFERENCES `ciudad` (`ciudad_id`);
+
+--
+-- Filtros para la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+  ADD CONSTRAINT `ciudad_ibfk_1` FOREIGN KEY (`departamento_id`) REFERENCES `departamento` (`departamento_id`);
+
+--
+-- Filtros para la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD CONSTRAINT `empresa_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`usuario_id`),
+  ADD CONSTRAINT `empresa_ibfk_2` FOREIGN KEY (`subcategoria_id`) REFERENCES `subcategoria` (`subcategoria_id`),
+  ADD CONSTRAINT `empresa_ibfk_3` FOREIGN KEY (`barrio_id`) REFERENCES `barrio` (`barrio_id`);
+
+--
+-- Filtros para la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD CONSTRAINT `imagen_ibfk_1` FOREIGN KEY (`empresa-id`) REFERENCES `empresa` (`empresa_id`);
+
+--
+-- Filtros para la tabla `subcategoria`
+--
+ALTER TABLE `subcategoria`
+  ADD CONSTRAINT `subcategoria_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`categoria_id`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`tipo_usuario_id`) REFERENCES `tipo_usuario` (`tipo_usuario_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
